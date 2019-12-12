932dc75108b9c78ae381104833a56038025f3d50
Adams Paul <adamsohiani@gmail.com>
2019-12-12 14:11:31 +0100

    UI and Framework

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -51,6 +51,17 @@
                                 </div>
                             </p>
                             <p>
+                                    <div class="col-md-6">
+                                    <input id="name" type="tel" class="form-control" name="phone_number" value="{{ old('phone_number') }}" required autofocus>
+                             
+                                 @if ($errors->has('phone_number'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('phone_number') }}</strong>
+                                    </span>
+                                @endif
+                                </div>
+                            </p>
+                            <p>
 
                                 <div class="">
                                     <input placeholder="Password" id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
